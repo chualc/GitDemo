9259455b7e03c9c3c3e053b589b4564f0f9ac22b
chualc <chua_li_chien@myaccount.nyp.edu.sg>
2019-01-15 16:19:20 +0800

    Add port number

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, Session
+import persistence
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def hello_world():
-    return 'Hello World!'
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
 
+        persistence.create_user(firstname, lastname)
+
+        return render_template('formInfo.html', un = firstname, ln=lastname)
+
+    return render_template('home.html')
+
+
+@app.route('/<string:firstname>/')
+def display_name(firstname):
+    userInfo = persistence.get_user(firstname)
+    return userInfo.firstname + ' ' + userInfo.lastname
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port='80')
